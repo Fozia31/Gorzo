70f961699520b79b3fe4b525672f216e4891aa42
Messibre <messibre27@gmail.com>
2026-03-07 16:03:20 +0200

    fixing bug

--- backend/lib/mongo.js
+++ backend/lib/mongo.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGO_URI || process.env.MONGODB_URI;
-
 if (!global._mongooseCache) {
   global._mongooseCache = { conn: null, promise: null };
 }
@@ -9,14 +7,16 @@ if (!global._mongooseCache) {
 const cache = global._mongooseCache;
 
 async function connectToMongo() {
-  if (!MONGO_URI) {
+  const mongoUri = process.env.MONGO_URI || process.env.MONGODB_URI;
+
+  if (!mongoUri) {
     throw new Error("MongoDB URI not found. Set MONGO_URI or MONGODB_URI.");
   }
 
   if (cache.conn) return cache.conn;
 
   if (!cache.promise) {
-    cache.promise = mongoose.connect(MONGO_URI).then((mongooseInstance) => mongooseInstance);
+    cache.promise = mongoose.connect(mongoUri).then((mongooseInstance) => mongooseInstance);
   }
 
   cache.conn = await cache.promise;
